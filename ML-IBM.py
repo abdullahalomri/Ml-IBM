2f3a7d6b452d76a18ada0ceeb03eff56cc6e2af0
abdullahalomri <106389482+abdullahalomri@users.noreply.github.com>
2024-08-27 20:14:54 +0300

    Update ML-IBM.py

--- ML-IBM.py
+++ ML-IBM.py
@@ -1,1 +1,102 @@
+Exercise 2 - Create a markdown cell with the title of the notebook. (1 pt)
+----------
+
+the title of this ducument is IBMCOU
+
+
+Exercise 3 - Create a markdown cell for an introduction. (1 pt)
+------
+In this notebook, Data Science Tools and Ecosystem are summarized.
+
+Objectives:
+
+List popular languages that Data Scientists use.
+List commonly used libraries used by Data Scientists.
+Comment on Data Science tools.
+
+Exercise 4 - Create a markdown cell to list data science languages. (3 pts)
+----------
+
+Some of the popular languages that Data Scientists use are:
+
+Python.
+R.
+SQL.
+Java.
+Julia.
+Scala.
+C/C++.
+JavaScript.
+
+Exercise 5 - Create a markdown cell to list data science libraries. (3 pts)
+------------
+Some of the commonly used libraries used by Data Scientists include:
+
+TensorFlow.
+NumPy.
+SciPy.
+Pandas.
+Matplotlib.
+Keras.
+SciKit-Learn.
+PyTorch.
+Scrapy.
+BeautifulSoup.
+LightGBM.
+ELI5.
+Theano.
+NuPIC.
+Ramp.
+Pipenv.
+Bob.
+PyBrain.
+Caffe2.
+Chainer.
+
+Exercise 6 - Create a markdown cell with a table of Data Science tools. (3 pts)
+-----------
+Data Science Tools:
+
+Data Science Tools		
+SAS. It is one of those data science tools which are specifically designed for statistical operation		
+Apache Spark		
+BigML
+
+Exercise 7 - Create a markdown cell introducing arithmetic expression examples. (1 pt)
+------------
+
+#sum
+5+6
+
+#sub
+5-6
+
+#mut
+5*5
+
+#divide
+5/5
+
+#Exercise 8 - Create a code cell to multiply and add numbers. (2 pts)
+5*5+5
+
+
+#Exercise 9 - Create a code cell to convert minutes to hours. (2 pts)
+sec=0
+min=0
+hour=0
+
+time=300
+hours_new=time/60
+print (hours_new)
+
+Exercise 10 -Insert a markdown cell to list Objectives. (3 pts)
+----------
+Below the introduction cell created in Exercise 3, insert a new markdown cell to list the objectives that this notebook covered (i.e. some of the key takeaways from the course). In this new cell start with an introductory line titled: Objectives: in bold font. Then using an unordered list (bullets) indicate 3 to 5 items covered in this notebook, such as List popular languages for Data Science.
+
+
+Exercise 11 - Create a markdown cell to indicate the Author’s name. (2 pts)
+---------
+Auther is Abdullah alomri
+
 
